e3d2a0dce802fb57b29518bae1720c0de5c4817e
vovacpp <vova.cpp@gmail.com>
2013-06-03 10:13:05 -0400

    flask admin

--- vova/task1/t9.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python2.7
-from sys import stdin, stdout
-
-s = stdin.readline()
-m, n, k, l = [int(x) for x in s.split()]
-
-if m == k or n == l or (m - n) == (k - l) or (m + n) == (k + l):
-	stdout.write("threatend")
-
